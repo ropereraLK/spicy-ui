9f848f9d161ab79998def23b69b3d694d082e2d5
Rohan Perera <ropereralk@gmail.com>
2024-06-25 19:53:40 +0530

    initial Commit'

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import Background from './components/global/background/Background.jsx'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+ 
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +19,8 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <Background/> 
     </div>
   );
 }
---
+++ src/components/global/background/Background.jsx
@@ -0,0 +1,10 @@
+import styles from './Background.module.css';
+
+export default function Background(){
+    return (
+        <div className={styles.div}>
+            <p>Hii</p>
+            <p>Hiiiii</p>
+        </div>
+        );
+}
